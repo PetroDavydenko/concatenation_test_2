3518a924f3a8f764f32330ee1ecc5da51a4eb578
Admin <petrodavidenko32@gmail.com>
2022-03-17 01:12:48 +0200

    Initial commit

---
+++ src/concatenation_test_2.java
@@ -0,0 +1,8 @@
+public class concatenation_test_2 {
+    public static void main(String[] args) {
+        String name = "Виктор";
+        String car = "Porshe 918 Spider";
+
+        System.out.println(name + " купил " + car);
+    }
+}
